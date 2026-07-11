6821539bcbfa47edaee0239e6593640e7ade909d
omkumarray <27omrajput@gmail.com>
2026-07-11 21:39:08 +0530

    addinhg new mini project files for HTML basics lectures

---
+++ DEV/HTML/BASICS/LECTURES/Mini Projects/script.js
@@ -0,0 +1,24 @@
+function addTask(){
+
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    if(task === ""){
+        alert("Please enter a task!");
+        return;
+    }
+
+    let li = document.createElement("li");
+    li.innerHTML = `
+        ${task}
+        <button class="delete" onclick="deleteTask(this)">Delete</button>
+    `;
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
+
+function deleteTask(button){
+    button.parentElement.remove();
+}
